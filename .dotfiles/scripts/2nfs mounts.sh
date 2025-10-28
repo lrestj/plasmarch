379b6f9854420d89083a91b6a94adc4b3a3d2f95
LrestJ <rest@seznam.cz>
2025-10-28 13:00:43 +0100

    Nfs fix

--- .dotfiles/scripts/2nfs mounts.sh
+++ .dotfiles/scripts/2nfs mounts.sh
@@ -1,29 +1,11 @@
 #KLIENT NASTAVENÍ:
 #!/bin/bash
 
-#sudo apt install nfs-common
-
-sudo systemctl enable --now nfs-client.target
-
-sudo systemctl enable --now NetworkManager-wait-online.service
-
-
-#Ověřit:
-
-sudo showmount -e 192.168.77.18
-
-
 #mounts
 
-sudo mkdir -p /data/nfs/FilmyNas /data/nfs/HudbaNas /data/nfs/Nas &&
+sudo mkdir -p /nfs/FilmyNas /nfs/HudbaNas /nfs/Nas &&
 
-sudo chmod -R ugo+rwx /data/nfs
-
-sudo mount -t nfs 192.168.77.18:/volume1/Rodinas /data/nfs/Nas
-
-sudo mount -t nfs 192.168.77.18:/volume1/Hudba /data/nfs/HudbaNas
-
-sudo mount -t nfs 192.168.77.18:/volume1/Filmy /data/nfs/FilmyNas
+sudo chmod -R ugo+rwx /nfs
 
 #add to fstab:
 sudo cp /etc/fstab /etc/fstab.bak
